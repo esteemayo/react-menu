a5657ad026749f2b87ec4dbb03eb8cbae8f6f4b2
Emmanuel Adebayo <eadebayo15@gmail.com>
2021-05-09 01:16:14 +0100

    Completed the react menu application

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, i) => (
+        <button
+          key={i}
+          onClick={() => filterItems(category)}
+          className="filter-btn"
+          type="button"
+        >
+          {category}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
