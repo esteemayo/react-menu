a33b2a40fb76981f8d2ca141e7dab3e32296e883
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-27 00:19:35 +0100

    fix: change file name

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 const menu = [
   {
     id: 1,
-    title: "buttermilk pancakes",
-    category: "breakfast",
+    title: 'buttermilk pancakes',
+    category: 'breakfast',
     price: 15.99,
     img: "./images/item-1.jpeg",
     desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
