6b8457ec95970f52a14d403f96fa10728736b7e5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-27 00:22:15 +0100

    fix: modify

--- src/data.js
+++ src/data.js
@@ -65,8 +65,8 @@ const menu = [
   },
   {
     id: 9,
-    title: "quarantine buddy",
-    category: "shakes",
+    title: 'quarantine buddy',
+    category: 'shakes',
     price: 16.99,
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
