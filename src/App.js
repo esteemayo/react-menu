616ef4b24f4bc3799ab0dd8ea0772eda880e04c4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-27 00:11:48 +0100

    fix: modify

--- src/App.js
+++ src/App.js
@@ -25,8 +25,8 @@ function App() {
 
   return (
     <main>
-      <section className="menu section">
-        <div className="title">
+      <section className='menu section'>
+        <div className='title'>
           <h2>Our Menu</h2>
           <div className="underline"></div>
         </div>
