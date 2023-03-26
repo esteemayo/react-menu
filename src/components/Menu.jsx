e97a741cee5435e45d4f38ba7260269beb845a92
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-27 00:15:46 +0100

    fix: modify

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -11,7 +11,7 @@ const Menu = ({ items }) => {
                 <h4>{title}</h4>
                 <h4 className='price'>${price}</h4>
               </header>
-              <p className="item-text">{desc}</p>
+              <p className='item-text'>{desc}</p>
             </div>
           </article>
         );
